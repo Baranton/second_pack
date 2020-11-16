5dd8066c879744f9cc6c995624187b7936e472fb
Baranton <anton.barthelmess@gmail.com>
2020-11-16 16:29:36 +0100

    corrected typos

--- tests/first_function_test.py
+++ tests/first_function_test.py
@@ -6,8 +6,6 @@ import second_pack
 from second_pack.first_function import first_function
 import pytest
 
-"hi my purpose in life is to help Anton complete this challenge because he's tired."
-
 def test_fist_function():
   test = "hi my purpose in life is to help Anton complete this challenge because he's tired."
   result = first_function()
